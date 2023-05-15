fc08be59a44da2388a7755e88fe94cce50e4c001
kodzekun <133255043+aKodzekun@users.noreply.github.com>
2023-05-16 01:16:52 +0800

    update login

--- kodzekun/kodtime/urls.py
+++ kodzekun/kodtime/urls.py
@@ -4,9 +4,12 @@ from .views import *
 
 urlpatterns = [
     path('', home, name="home"),
-    path('registration/', registration, name="registration"),
-    path('login/', login, name="login"),
     path('sign/', sign, name="sign"),
+    path('login/', login, name="login"),
+    path('signClick/', signClick, name="signClick"),
+    path('exitClick/', exitClick, name="exitClick"),
     path('loginClick/', loginClick, name="loginClick"),
+    path('signtologin/', signtologin, name="signtologin"),
+    path('logintosign/', logintosign, name="logintosign")
 
 ]
