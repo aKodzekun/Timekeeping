69c519c975ac9e75fa1578763cdcac1a0cc0d93c
kodzekun <133255043+aKodzekun@users.noreply.github.com>
2023-05-16 11:45:14 +0800

    update login

--- kodzekun/kodtime/urls.py
+++ kodzekun/kodtime/urls.py
@@ -9,7 +9,14 @@ urlpatterns = [
     path('signClick/', signClick, name="signClick"),
     path('exitClick/', exitClick, name="exitClick"),
     path('loginClick/', loginClick, name="loginClick"),
+    path('dashboard/', dashboardClick, name="dashboardClick"),
+    path('notificationClick/', notificationClick, name="notificationClick"),
+    path('analystClick/', analystClick, name="analystClick"),
+    path('memberClick/', memberClick, name="memberClick"),
+    path('settingsClick/', settingsClick, name="settingsClick"),
     path('signtologin/', signtologin, name="signtologin"),
-    path('logintosign/', logintosign, name="logintosign")
-
+    path('logintosign/', logintosign, name="logintosign"),
+    path('forgetClick/', forgetClick, name="forgetClick"),
+    path('logintoforget/', logintoforget, name="logintoforget"),
+    path('password_recovery/', password_recovery, name="password_recovery")
 ]
